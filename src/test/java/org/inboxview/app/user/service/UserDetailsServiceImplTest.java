33dd6de9b3ce64f3a9761a0e8f942b7391288563
George <nabuage@outlook.com>
2025-07-25 23:43:03 -0500

    Add user details tests.

---
+++ src/test/java/org/inboxview/app/user/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,79 @@
+package org.inboxview.app.user.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.inboxview.app.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class UserDetailsServiceImplTest {
+    @InjectMocks
+    UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    UserRepository userRepository;
+
+    UserDetails userDetails;
+    org.inboxview.app.user.entity.User user;
+
+    @BeforeEach
+    public void setup() {
+        userDetails = User.builder()
+            .username("username")
+            .password("password")
+            .build();
+        
+        user = org.inboxview.app.user.entity.User .builder()
+            .id(1L)
+            .username("username")
+            .password("password")
+            .email("email@inboxview.com")
+            .firstName("firstname")
+            .lastName("lastname")
+            .build();
+    }
+
+    @Test
+    public void testLoadUserByUsernameReturnsUserDetails() {
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var result = userDetailsService.loadUserByUsername(user.getUsername());
+
+        assertThat(result).isEqualTo(userDetails);
+
+        verify(userRepository, times(1)).findByUsername(anyString());
+    }
+
+    @Test
+    public void testRegisterReturnsUsernameNotFoundException() {
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.empty());
+
+        Exception result = assertThrows(UsernameNotFoundException.class, () -> {
+            userDetailsService.loadUserByUsername(user.getUsername());
+        });
+
+        assertThat(result.getMessage()).isEqualTo("Username is not found.");
+
+        verify(userRepository, times(1)).findByUsername(anyString());
+    }
+    
+}
